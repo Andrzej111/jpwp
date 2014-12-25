b2fd2c0408b31aa746a5f9bb3a89e7164fb208ef
Paweł Szulc <pawel_szulc@onet.pl>
2014-12-25 18:28:13 +0100

    początki

---
+++ jpwp/www/js/main.js
@@ -0,0 +1,16 @@
+
+//	#	#	#	#	///
+
+			// var p=new place("Studio","No studio no",50.068137, 19.907989);
+			// var p2=new place("Kapitol","Kapitol",50.067841, 19.907050);
+			// var p3=new place("Lewiatan :)","Opis",50.068636, 19.907549);
+			// var p4=new place("D5","Podpis",50.067124, 19.915406);
+			// var p5=new place("Jakaś bardzo długa nazwa ciekawe co się stanie","No kurde biegun północny",90, 0);
+			// my_places=[p, p5, p3, p4, p2];
+
+$(document).ready(function(){
+    $.support.cors = true;
+	$.mobile.allowCrossDomainPages=true;	//jak potrzeba ładować zewnętrzne strony
+	$.mobile.phonegapNavigationEnabled = true;
+    
+});
