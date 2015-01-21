79c56bbd64957024579fd8e82e94c12216cae78b
Paweł Szulc <pawel_szulc@onet.pl>
2015-01-21 23:32:32 +0100

    Finito

--- jpwp/platforms/wp8/www/js/funkcje.js
+++ jpwp/platforms/wp8/www/js/funkcje.js
@@ -44,3 +44,151 @@ function grid3_maker(left,mid,right){
 		return grid;
 	}
 }
+
+function gridsolo_maker(left){
+	var grid=$('<div class=ui-grid-solo></div>');
+	if (typeof(left)=="object"){
+		for (var i=0; i<left.length;i++){
+			$(grid).append('<div class=ui-block-a>'+left[i]+'</div>');
+		}
+		return grid;
+	} 
+	else {
+		$(grid).append('<div class=ui-block-a>'+left+'</div>');
+		return grid;
+	}
+	
+}
+
+// function wyslij_email(adres_email){
+//             if (window.plugin) {
+//                 window.plugin.email.isServiceAvailable(function (isAvailable) {
+//                     if (isAvailable) {
+//                         window.plugin.email.open({
+//                             to: [adres_email],
+//                             subject: 'Email testowy',
+//                             body: 'To jest email testowy',
+//                         });
+//                     }
+//                     else {
+//                         alert("Funkcja niedostępna");
+//                     }
+//                 });
+//             }
+// }
+
+// function wyslij_sms(numer){
+
+//         var messageInfo = {
+//             phoneNumber: numer,
+//             textMessage: "Wpisz tekst wiadomości"
+//         };
+
+//         sms.sendMessage(messageInfo, function (message) {
+//             console.log("success: " + message);
+//         }, function (error) {
+//             console.log("code: " + error.code + ", message: " + error.message);
+//         });
+
+// }
+
+function podepnij_guziki(){
+        $('a.wyslij-email').click(function () {
+            var adres_email=$(this).attr('mail');
+            if (window.plugin) {
+                window.plugin.email.isServiceAvailable(function (isAvailable) {
+                    if (isAvailable) {
+                        window.plugin.email.open({
+                            to: [adres_email],
+                            subject: 'Email testowy',
+                            body: 'To jest email testowy',
+                        });
+                    }
+                    else {
+                        alert("Funkcja niedostępna");
+                    }
+                });
+            }
+        });
+    // guziki z klasą wyslij-sms będą wysyłały smsa (wow2!) na numer zadany w atrybucie numer (zaskoczenie!)
+    $('a.wyslij-sms').click(function () {
+        var numer = $(this).attr('numer');
+
+        var messageInfo = {
+            phoneNumber: numer,
+            textMessage: "Wpisz tekst wiadomości"
+        };
+
+        sms.sendMessage(messageInfo, function (message) {
+            console.log("success: " + message);
+        }, function (error) {
+            console.log("code: " + error.code + ", message: " + error.message);
+        });
+    });
+
+}
+
+function wyswietl_dane(id){
+	            $.getJSON('http://pluton.kt.agh.edu.pl/~pszulc/jpwp/get_pacjent_dane.php',
+                {"id":id},
+                function(response){
+                    var data=response.dane;
+
+                    var grid=gridsolo_maker(["Imię: "+data.imie,"Nazwisko: "+data.nazwisko,"PESEL: "+data.pesel,"Nr telefonu: "+data.tel,"Adres E-mail: "+data.email]);
+                    $("#dane").attr('pacjent-id',data.id);
+                    //alert($("#dane").attr('pacjent-id'));
+                    $("#dane .ui-content").html(
+                        $(grid)
+                    );
+                    var link1=$('<a class="wyslij-sms ui-btn ui-icon-comment ui-btn-icon-left" >Wyślij SMS</a>').attr('numer',data.tel);
+                    var link2=$('<a class="wyslij-email ui-btn ui-icon-mail ui-btn-icon-left" >Wyślij E-Mail</a>').attr('mail',data.email);
+
+                    $("#dane .ui-content").append(  $('<a class="ui-btn ui-icon-phone ui-btn-icon-left">Zadzwoń</a>').attr('href',"tel:"+data.tel) );                                 
+                    $("#dane .ui-content").append( $(link1) );
+                    $("#dane .ui-content").append( $(link2) );
+
+                    podepnij_guziki();
+
+                    var wyniki=response.badania;
+                    //alert(JSON.stringify(data));
+                    $("#lista-badan-pacjenta").html('');
+                    if (wyniki){
+                        $.each(wyniki,function(index,value){
+                            var collapsible={};
+
+                            if(parseFloat(value.wynik)>parseFloat(value.g_kryt) || parseFloat(value.wynik)<parseFloat(value.d_kryt) ){
+                            	collapsible=$('<div data-role="collapsible" data-theme="c" data-collapsed-icon="alert" data-expanded-icon="alert"><h1>'+value.nazwa+'</h1></div>');
+                                if(parseFloat(value.wynik)>parseFloat(value.g_kryt)){
+                                    $(collapsible).append("<p>Wynik przekroczył górną wartość krytyczną "+value.g_kryt+value.jednostka+"</p>");
+                                } else {
+                                    $(collapsible).append("<p>Wynik przekroczył dolną wartość krytyczną "+value.d_kryt+value.jednostka+"</p>");   
+                                }
+                        	} else if (parseFloat(value.wynik)>parseFloat(value.g_alert) || parseFloat(value.wynik)<parseFloat(value.d_alert)) {
+                        		//alert(value.wynik+" "+value.g_kryt+" "+value.d_kryt);
+                            	collapsible=$('<div data-role="collapsible" data-theme="d" data-collapsed-icon="info" data-expanded-icon="info"><h1>'+value.nazwa+'</h1></div>');
+                                if(parseFloat(value.wynik)>parseFloat(value.g_alert)){
+                                    $(collapsible).append("<p>Wynik przekroczył górną wartość ostrzegawczą "+value.g_alert+value.jednostka+"</p>");
+                                } else {
+                                    $(collapsible).append("<p>Wynik przekroczył dolną wartość ostrzegawczą "+value.d_alert+value.jednostka+"</p>"); 
+                                }
+                        	} else {
+                        		//alert(value.wynik+" "+value.g_kryt+" "+value.d_kryt);
+                            	collapsible=$('<div data-role="collapsible" data-collapsed-icon="check" data-expanded-icon="check"><h1>'+value.nazwa+'</h1></div>');
+                        	}
+
+                        $(collapsible).append(grid2_maker(["Wynik:","Data:"],[value.wynik+value.jednostka,value.data]));
+                            $("#lista-badan-pacjenta").append($(collapsible));
+                        });
+                    } else {
+                    	$("#lista-badan-pacjenta").append('<p>Brak przeprowadzonych badań</p>');
+                    };
+                    $("#lista-badan-pacjenta").collapsibleset( "refresh" );
+                    //alert(JSON.stringify(data));
+
+
+                    $('#lista-pacjentow #filtr-listy').val(''); // wyczyść filtr
+                }
+
+            );
+
+}
--- jpwp/www/js/main.js
+++ jpwp/www/js/main.js
@@ -2,28 +2,32 @@ $(document).ready(function(){
     $.support.cors = true;
 	$.mobile.allowCrossDomainPages=true;	//jak potrzeba ładować zewnętrzne strony
 	$.mobile.phonegapNavigationEnabled = true;
-	$.ajaxSetup({
-		timeout: 100000
-	});
+	// $.ajaxSetup({
+	// 	timeout: 100000
+	// });
 
     console.log("Document Ready");
-    $('a.wyslij-email').click(function () {
-        if (window.plugin) {
-            window.plugin.email.isServiceAvailable(function (isAvailable) {
-                if (isAvailable) {
-                    window.plugin.email.open({
-                        to: ['pawel_szulc@onet.pl'],
-                        subject: 'Email testowy',
-                        body: 'To jest email testowy',
-                    });
-                }
-                else {
-                    alert("Funkcja niedostępna");
-                }
-            });
-        }
-    });
 
+
+    // guziki z klasą wyslij-email będą wysyłały email (wow!) na adres zawarty w atrybucie mail (niewiarygodne!) 
+        $('a.wyslij-email').click(function () {
+            var adres_email=$(this).attr('mail');
+            if (window.plugin) {
+                window.plugin.email.isServiceAvailable(function (isAvailable) {
+                    if (isAvailable) {
+                        window.plugin.email.open({
+                            to: [adres_email],
+                            subject: 'Email testowy',
+                            body: 'To jest email testowy',
+                        });
+                    }
+                    else {
+                        alert("Funkcja niedostępna");
+                    }
+                });
+            }
+        });
+    // guziki z klasą wyslij-sms będą wysyłały smsa (wow2!) na numer zadany w atrybucie numer (zaskoczenie!)
     $('a.wyslij-sms').click(function () {
         var numer = $(this).attr('numer');
 
@@ -41,7 +45,9 @@ $(document).ready(function(){
 
     sort_ul("#lista-pacjentow ul");
 
-	$(document).on("pagebeforeshow", "#lista-pacjentow", function () {
+
+    // wczytuje listę pacjentów z serwera
+	$(document).on("pageshow", "#lista-pacjentow", function () {
         $('#lista-pacjentow #filtr-listy').val('');
 		$('#lista-pacjentow ul').children().hide();
     	$('#lista-pacjentow ul').children().remove();
@@ -55,14 +61,108 @@ $(document).ready(function(){
     				$.each(data,function(index,value){
     					var filter=value.nazwisko+" "+value.imie+" "+value.pesel;
     					var li=$('<li data-filtertext="'+filter+'"></li>');
-    					//$(li).attr('id',value.id);
-    					$(li).append($('<a>'+value.nazwisko+' '+value.imie+'</a>').attr('id',value.id));
+                        //$(li).attr('id',value.id);
+                        $(li).append($('<a href="#dane">' + value.nazwisko + ' ' + value.imie + '</a>').attr('pacjent-id', value.id));
+                        $(li).attr('pacjent-id', value.id);
+                        $(li).click(function(){
+                            var id=$(this).attr('pacjent-id');
+                            wyswietl_dane(id);
+                        });
     					$('#lista-pacjentow ul').append(li);
     				});
-    				$('#lista-pacjentow ul').show();//listview('refresh')
+    				$('#lista-pacjentow ul').show();
     				$('#lista-pacjentow ul').listview('refresh');
     			}
     		}
-    	);
-	});      
+    	);//koniec tworzenia listy pacjentów
+
+	});     
+
+    $(document).on("pageshow", "#lista-krytyczna", function () {
+        $.getJSON('http://pluton.kt.agh.edu.pl/~pszulc/jpwp/lista_krytyczna.php',
+            function(data){
+                $("#ul-krytyczna").html('');
+                $.each(data,function(index,value){
+                    var collapsible={};
+                    collapsible=$('<li data-theme="c"><a href="#historia-badan" data-icon="alert" dataiconpos="left">'+value.nazwisko+" "+value.data+'</a></li>');
+                    $(collapsible).attr('pacjent-id',value.id);
+                    $(collapsible).click(function(){
+                        var id=$(this).attr('pacjent-id');
+                        wyswietl_dane(id);
+                    });
+                    $('#ul-krytyczna').append($(collapsible));
+
+                });
+                $("#ul-krytyczna").listview('refresh');
+            }
+
+        );
+    }); 
+
+    $('#dodaj-wynik-button').click(function(){
+        //alert($("#dane").attr('pacjent-id'));
+        var id=$("#dane").attr('pacjent-id');
+        //$('#wpisz-wynik').attr('id',id);
+        $("#wpisz-wynik-id").val(id);
+        $.getJSON('http://pluton.kt.agh.edu.pl/~pszulc/jpwp/get_badania.php',
+            function(data){
+                $.each(data,function(index,value){
+                    var option={};
+                    option=$('<option value='+value.id+'>'+value.nazwa+" ["+value.jednostka+']</a></li>');
+                    $('#wpisz-wynik-badanie').append($(option));
+                });
+            }
+
+        );
+
+    });
+
+    $("#wpisz-wynik-badanie").change(function(){
+        var tekst=$("#wpisz-wynik-badanie :selected").text();
+        var jednostka=tekst.replace(/.*?\[/,"[");
+         $('label[for="wpisz-wynik-wartosc"]').text("Wpisz wynik "+jednostka);
+    });
+
+    $('form#wpisz-wynik-form').on("submit",function(event){
+        event.preventDefault();
+        alert($("#wpisz-wynik-wartosc").val());
+        $.post("http://pluton.kt.agh.edu.pl/~pszulc/jpwp/wpisz_wynik.php",
+            {
+            'wpisz-wynik-id':$("#wpisz-wynik-id").val(),
+            'wpisz-wynik-badanie':$("#wpisz-wynik-badanie").val(),
+            'wpisz-wynik-wartosc':$("#wpisz-wynik-wartosc").val()
+            },
+             function(data){
+                if (data.err){alert(data.err);} else {
+                    alert(data.sukces);
+                }    
+            },
+            "json"  
+        );
+    });
+        
+    $('form#nowy-pacjent-form').on("submit",function(event){
+        event.preventDefault();
+        //alert($("#wpisz-wynik-wartosc").val());
+        $.post("http://pluton.kt.agh.edu.pl/~pszulc/jpwp/nowy_pacjent.php",
+            {
+            'imie':$("#nowy-pacjent-imie").val(),
+            'nazwisko':$("#nowy-pacjent-nazwisko").val(),
+            'pesel':$("#nowy-pacjent-pesel").val(),
+            'plec':$("#nowy-pacjent-plec").val(),
+            'tel':$("#nowy-pacjent-tel").val(),
+            'email':$("#nowy-pacjent-email").val()
+            },
+             function(data){
+                if (data.err){alert(data.err);} else {
+                    alert(data.sukces);
+                }    
+            },
+            "json"  
+        );
+    });
+
+    //  $(document).on("pageshow", "#wpisz-wynik", function () {
+        
+    // }); 
 });
